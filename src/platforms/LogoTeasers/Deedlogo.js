88ca463216cd9d8f73897085338cf3e0d194032c
Patrik Kozak <patrik9@comcast.net>
2021-01-25 15:36:41 -0500

    Changed name of ArcDeed logo file

--- src/platforms/LogoTeasers/Deedlogo.js
+++ src/platforms/LogoTeasers/Deedlogo.js
@@ -10,7 +10,7 @@ const deed= {
 const DeedLogo = () => {
     return (
         <div className="mt-2 mb-2">
-          <img style={deed} className="img-fluid" src={require('../../img/AD3.png')} alt=""/>
+          <img style={deed} className="img-fluid" src={require('../../img/ArcDeed3.png')} alt=""/>
         </div>
 
         );

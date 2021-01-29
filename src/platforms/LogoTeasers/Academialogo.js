0049cb7c451cf76e300254b7f390dcbbee5ded7d
Patrik Kozak <patrik9@comcast.net>
2021-01-29 11:42:50 -0500

    Fixed alignment of academ logo in mobile

--- src/platforms/LogoTeasers/Academialogo.js
+++ src/platforms/LogoTeasers/Academialogo.js
@@ -9,7 +9,7 @@ const aca = {
 
 const AcademiaLogo = () => {
     return (
-        <div className="mt-2 mb-2">
+        <div className="mt-2 mb-2 academ-logo-shift">
             <img style={aca} className="img-fluid" src={require('../../img/AA3.png')} alt=""/>
 
         </div>
